c3b415b22aa3b5bf5efc771e87f9dff75b5ad7dc
Marcus Geil <mg@writereader.com>
2017-02-02 08:51:09 +0100

    Initial commit

---
+++ movie-db.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Movie DB
+Description: Registers a custom post type for movies. Bascially, make your own movie database.
+Version: 0.0.1
+Author: Marcus Asbæk Geil
+Author URI: http://marcusis.me
+License: MIT
+Text Domain: mdb
+*/
